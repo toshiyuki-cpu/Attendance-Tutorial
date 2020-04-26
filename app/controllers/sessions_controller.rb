07f6ca7b7273c064f814832709ea9efb24e6f001
toshiyuki <spd36bq9@gmail.com>
2020-04-26 15:46:32 +0000

    7章終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
+
   def new
   end
-  
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -10,12 +11,12 @@ class SessionsController < ApplicationController
       redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
-    render :new
+      render :new
     end
   end
-  
+
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
   end
